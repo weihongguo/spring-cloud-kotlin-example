2ff24966f052ab0e272891bbe034ee80915102c2
guoguo <35117668@qq.com>
2020-05-07 15:20:49 +0800

    config and eureka

---
+++ config/src/main/kotlin/com/example/config/ConfigApplication.kt
@@ -0,0 +1,15 @@
+package com.example.config
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.config.server.EnableConfigServer
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient
+
+@SpringBootApplication
+@EnableConfigServer
+@EnableEurekaClient
+class ConfigApplication
+
+fun main(args: Array<String>) {
+	runApplication<ConfigApplication>(*args)
+}
